df86f36f080b927a090965cde7222c3e49f9fb32
Sacura <Sacuramec1@gmail.com>
2025-07-14 11:19:23 +0100

    fix port

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+const port = process.env.PORT ? parseInt(process.env.PORT) : 10000;
+
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0',
+    port
+  },
+  preview: {
+    host: '0.0.0.0',
+    port,
+    allowedHosts: true,
+  }
+});
