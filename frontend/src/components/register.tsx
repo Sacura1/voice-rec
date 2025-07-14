1a9e673d15ca262b6b602861a4c474d8b7025468
Sacura <Sacuramec1@gmail.com>
2025-07-14 12:17:31 +0100

    save changes

--- frontend/src/components/register.tsx
+++ frontend/src/components/register.tsx
@@ -26,7 +26,7 @@ const Register = () => {
         e.preventDefault()
 
         try {
-        const   response  = await axios.post('http://localhost:3000/register', form,
+        const   response  = await axios.post(`${import.meta.env.VITE_API_URL}/register`, form,
             {withCredentials:true}
         )
         console.log(response.data.user.username)
